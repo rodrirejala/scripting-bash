54457a950dda1d7e4be7935c41bfdb7048d10cb6
Rodrigo Rejala <rodrirejala@gmail.com>
2026-04-05 21:16:35 -0300

    primer script bash

---
+++ intro.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+echo "Hola Mundo!"
+echo $(which nqn)
+cat $(which neqn)
+
+# Recuerda antes de poder ejecutar el script bash como un programa
+# hay que darle los permisos esto se hace con el comando:
+# chmod +x intro
+# y para poder ejecutar el script recuerda punto slash:
+# ./intro.sh
+
